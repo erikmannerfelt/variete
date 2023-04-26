9a809e4cd90dbedd0d1c760bd6e502e13ce2df15
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2023-04-26 11:33:11 +0200

    Added development nix flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonpkgs = pkgs.python310Packages;
+
+        python_with_pkgs = (pythonpkgs.python.withPackages (_: with pythonpkgs; [
+          python
+          rasterio
+          gdal
+          pytest
+          flake8
+          matplotlib
+          sphinx-book-theme
+          sphinx
+          numpydoc
+          myst-parser
+          #myst-nb
+          sphinxcontrib-programoutput
+          sphinx-autodoc-typehints
+          sphinx-design
+
+        ])).overrideAttrs (prev: {
+          pname = "python";
+        });
+
+        packages = builtins.listToAttrs (map (pkg: { name = pkg.pname; value = pkg; }) ( with pkgs; [
+          python_with_pkgs
+          pre-commit
+        ]));
+
+      in
+      {
+        inherit packages;
+        defaultPackage = packages.python;
+
+        devShell = pkgs.mkShell {
+          name = "variete";
+          buildInputs = pkgs.lib.attrValues packages;
+        };
+      }
+
+    );
+}
